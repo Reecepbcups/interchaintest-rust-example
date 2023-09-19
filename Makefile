0fdb7a601e0478e42233f8b632055aa9d157cd83
Reece Williams <reecepbcups@gmail.com>
2023-09-19 13:27:47 -0500

    Add working ictest rust example

---
+++ Makefile
@@ -0,0 +1,38 @@
+#!/usr/bin/make -f
+VERSION := $(shell echo $(shell git describe --tags) | sed 's/^v//')
+COMMIT := $(shell git log -1 --format='%H')
+
+CURRENT_DIR := $(shell pwd)
+BASE_DIR := $(shell basename $(CURRENT_DIR))
+
+compile:
+	@echo "Compiling Contract $(COMMIT)..."	
+	@docker run --rm -v "$(CURRENT_DIR)":/code \
+	--mount type=volume,source="$(BASE_DIR)_cache",target=/code/target \
+	--mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+	cosmwasm/rust-optimizer:0.12.13
+
+all:
+	cargo schema	
+	cargo fmt
+	cargo test
+	cargo clippy -- -D warnings
+
+clippy-pedantic:	
+	cargo clippy -- -W clippy::pedantic -W clippy::missing-errors-doc -W clippy::doc-markdown -W clippy::module-name-repetitions
+clippy-format:	
+	cargo clippy -- -W clippy::useless_format
+clippy-correctness:	
+	cargo clippy -- -W clippy::correctness
+clippy-suspicious:	
+	cargo clippy -- -W clippy::suspicious
+clippy-style:	
+	cargo clippy -- -W clippy::style
+clippy-perf:	
+	cargo clippy -- -W clippy::perf
+clippy-complexity:	
+	cargo clippy -- -W clippy::complexity
+clippy-new:	
+	cargo clippy -- -W clippy::nursery	
+
+# -W clippy::missing-errors-doc
--- interchaintest/src/main.rs
+++ interchaintest/src/main.rs
@@ -1,3 +1,77 @@
-fn main() {
-    println!("Hello, world!");
+use cosmwasm_std::Coin;
+use cosmwasm_std::Uint128;
+use localic_std::cosmwasm::CosmWasm;
+use reqwest::blocking::Client;
+use serde_json::json;
+
+use localic_std::balances::*;
+use localic_std::bank::*;
+use localic_std::polling::*;
+use localic_std::transactions::*;
+
+const API_URL: &str = "http://127.0.0.1:8080";
+
+fn main() {    
+    let client = Client::new();
+    poll_for_start(client.clone(), &API_URL, 150);
+
+    let rb: ChainRequestBuilder = ChainRequestBuilder::new(API_URL.to_string(), "localjuno-1".to_string(), true, false);
+
+    test_cosmwasm(&rb);
+}
+
+fn test_cosmwasm(rb: &ChainRequestBuilder) {
+    let cw = CosmWasm::new(&rb);
+
+    // sudo chmod -R +rwx artifacts/
+    let file_path = get_contract_path().join("ictest_rust.wasm");    
+    let code_id = cw.store_contract("acc0", file_path);    
+    if code_id.is_err() {        
+        panic!("code_id error: {:?}", code_id);
+    }    
+
+    let code_id = code_id.unwrap_or_default();
+    if code_id == 0 {
+        panic!("code_id is 0");
+    }
+
+    let msg = r#"{"count":0}"#;
+    let res = cw.instantiate_contract(
+        "acc0",
+        code_id,
+        msg,
+        "my-label",
+        Some("juno1hj5fveer5cjtn4wd6wstzugjfdxzl0xps73ftl"),
+        "",
+    );
+    println!("res: {:?}", res);
+
+    let contract = match res {
+        Ok(contract) => contract,
+        Err(err) => {
+            println!("err: {}", err);
+            return;
+        }
+    };
+
+    let prev_res = cw.query_contract(&contract.address, r#"{"get_count":{}}"#);
+    assert_eq!(prev_res, json!({"data":{"count":0}}));
+    println!("prev_res: {}", prev_res);
+
+    let data = cw.execute_contract(&contract.address, "acc0", r#"{"increment":{}}"#, "--gas=auto --gas-adjustment=2.0");
+    println!("unwrap: {}", data.unwrap());
+
+    let updated_res = cw.query_contract(&contract.address, r#"{"get_count":{}}"#);    
+    assert_eq!(updated_res, json!({"data":{"count":1}}));
+    println!("updated_res: {}", updated_res);
+}
+
+fn parent_dir() -> std::path::PathBuf {
+    return std::env::current_dir().unwrap().parent().unwrap().to_path_buf();
+}
+
+// get_contract_path returns the artifacts dire from parent_dir
+fn get_contract_path() -> std::path::PathBuf {
+    let path = parent_dir().join("artifacts");    
+    path
 }
